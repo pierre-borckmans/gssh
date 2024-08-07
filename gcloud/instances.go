ced6c87585f93bb35e2c2cbd89c92f00b950bdf6
Pierre Borckmans <pborckmans@gmail.com>
2024-08-07 15:31:49 +0200

    Save connection history + add exclusion list

--- gcloud/instances.go
+++ gcloud/instances.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"path"
 	"strings"
+	"time"
 )
 
 type InstanceStatus string
@@ -24,6 +25,12 @@ type Instance struct {
 	Status InstanceStatus
 }
 
+type Connection struct {
+	ConfigName string
+	Instance   *Instance
+	Timestamp  time.Time
+}
+
 var _ list.Item = &Instance{}
 
 func (i *Instance) Title() string       { return i.Name }
@@ -32,40 +39,104 @@ func (i *Instance) FilterValue() string {
 	return i.Name
 }
 
+var cacheDir string
+var historyFile string
+var history []*Connection
+var exclusionsFile string
+var exclusions []string
+
+func init() {
+	userConfigDir, _ := os.UserHomeDir()
+	cacheDir = path.Join(userConfigDir, ".gssh")
+	_ = os.MkdirAll(cacheDir, 0755)
+
+	// load or create the history file
+	historyFile = path.Join(cacheDir, "history.json")
+	_, err := os.Stat(historyFile)
+	if os.IsNotExist(err) {
+		f, _ := os.Create(historyFile)
+		defer func() {
+			_ = f.Close()
+		}()
+		_, _ = f.WriteString("[]")
+	}
+	history = make([]*Connection, 0)
+	bytes, err := os.ReadFile(historyFile)
+	_ = json.Unmarshal(bytes, &history)
+
+	// load or create the exclusions file
+	exclusionsFile = path.Join(cacheDir, "exclusions")
+	_, err = os.Stat(exclusionsFile)
+	if os.IsNotExist(err) {
+		f, _ := os.Create(exclusionsFile)
+		defer func() {
+			_ = f.Close()
+		}()
+		_, _ = f.WriteString("")
+	}
+	bytes, err = os.ReadFile(exclusionsFile)
+	if err == nil {
+		exclusions = strings.Split(string(bytes), "\n")
+	}
+	validExclusions := make([]string, 0)
+	for _, ex := range exclusions {
+		if strings.Trim(ex, " ") != "" {
+			validExclusions = append(validExclusions, ex)
+		}
+	}
+	exclusions = validExclusions
+}
+
 func ListInstances(configName string) ([]*Instance, error) {
-	cacheFile := fmt.Sprintf("instances_cache_%v.json", configName)
+	var instances []*Instance
+
+	cacheFile := path.Join(cacheDir, fmt.Sprintf("instances_cache_%v.json", configName))
 	if cached, err := os.ReadFile(cacheFile); err == nil {
-		var instances []*Instance
-		if json.Unmarshal(cached, &instances) == nil {
-			return instances, nil
+		_ = json.Unmarshal(cached, &instances)
+	}
+
+	if instances == nil || len(instances) == 0 {
+		cmd := exec.Command("gcloud", "compute", "instances", "list", "--format=json", "--configuration", configName)
+		output, err := cmd.Output()
+		if err != nil {
+			return nil, err
+		}
+
+		var rawInstances []map[string]interface{}
+		if err = json.Unmarshal(output, &rawInstances); err != nil {
+			return nil, err
+		}
+
+		instances = make([]*Instance, len(rawInstances))
+		for i, raw := range rawInstances {
+			instances[i] = &Instance{
+				Name:   raw["name"].(string),
+				Zone:   raw["zone"].(string),
+				Status: InstanceStatus(raw["status"].(string)),
+			}
 		}
 	}
 
-	cmd := exec.Command("gcloud", "compute", "instances", "list", "--format=json", "--configuration", configName)
-	output, err := cmd.Output()
-	if err != nil {
-		return nil, err
-	}
-
-	var rawInstances []map[string]interface{}
-	if err = json.Unmarshal(output, &rawInstances); err != nil {
-		return nil, err
-	}
-
-	instances := make([]*Instance, len(rawInstances))
-	for i, raw := range rawInstances {
-		instances[i] = &Instance{
-			Name:   raw["name"].(string),
-			Zone:   raw["zone"].(string),
-			Status: InstanceStatus(raw["status"].(string)),
+	filteredInstances := make([]*Instance, 0)
+	for _, inst := range instances {
+		excluded := false
+		for _, ex := range exclusions {
+			if strings.Contains(inst.Name, ex) {
+				excluded = true
+				continue
+			}
 		}
+		if excluded {
+			continue
+		}
+		filteredInstances = append(filteredInstances, inst)
 	}
 
-	cacheData, _ := json.Marshal(instances)
+	cacheData, _ := json.Marshal(filteredInstances)
 	if err := os.WriteFile(cacheFile, cacheData, 0644); err != nil {
 	}
 
-	return instances, nil
+	return filteredInstances, nil
 }
 
 func (i *Instance) SSH(configName string) error {
@@ -75,6 +146,19 @@ func (i *Instance) SSH(configName string) error {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	// add the connection to the history
+	conn := &Connection{
+		ConfigName: configName,
+		Instance:   i,
+		Timestamp:  time.Now(),
+	}
+	history = append(history, conn)
+	bytes, err := json.Marshal(history)
+	if err == nil {
+		_ = os.WriteFile(historyFile, bytes, 0644)
+	}
+
 	if err := cmd.Run(); err != nil {
 		return err
 	}
